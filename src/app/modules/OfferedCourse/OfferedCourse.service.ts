49f35afec310e9679eafc40212e24376cc3ab1ca
Akahad1 <aksahad234567@gmail.com>
2024-07-02 22:16:34 +0600

    add logic

--- src/app/modules/OfferedCourse/OfferedCourse.service.ts
+++ src/app/modules/OfferedCourse/OfferedCourse.service.ts
@@ -2,19 +2,72 @@ import httpStatus from "http-status";
 
 import { TOfferedCourse } from "./OfferedCourse.interface";
 import { OfferedCourse } from "./OfferedCourse.model";
+import { SemesterRegistration } from "../semesterRegistration/semesterRegistration.model";
+import { AppError } from "../../error/AppError";
+import { AcademicFaculty } from "../academicFaculty/academicFaculty.moldel";
+import { AcademicDepertment } from "../academicDepertment/academicDepertment.model";
+import { Course } from "../Course/course.model";
+import { Faculty } from "../Faculty/faculty.model";
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
-  //   const {
-  //     semesterRegistration,
-  //     academicFaculty,
-  //     academicDepartment,
-  //     course,
-  //     section,
-  //     faculty,
-  //     days,
-  //     startTime,
-  //     endTime,
-  //   } = payload;
+  const {
+    semesterRegistration,
+    academicFaculty,
+    academicDepartment,
+    course,
+    faculty,
+    section,
+    days,
+    startTime,
+    endTime,
+  } = payload;
+
+  const issemesterRegistrationExsist =
+    await SemesterRegistration.findById(semesterRegistration);
+  const academicSemester = issemesterRegistrationExsist?.academicSemester;
+
+  if (!issemesterRegistrationExsist) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Semseter Registar is not Exsist"
+    );
+  }
+  const isacademicFacultyExsist =
+    await AcademicFaculty.findById(academicFaculty);
+
+  if (!isacademicFacultyExsist) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Acdemic Faculty  is not Exsist"
+    );
+  }
+  const isacademicDepartmentExsist =
+    await AcademicDepertment.findById(academicDepartment);
+
+  if (!isacademicDepartmentExsist) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      "Acdemic Department is not Exsist"
+    );
+  }
+  const iscourseExsist = await Course.findById(course);
+
+  if (!iscourseExsist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Coures is not Exsist");
+  }
+  const isfacultyExsist = await Faculty.findById(faculty);
+
+  if (!isfacultyExsist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Faculty is not Exsist");
+  }
+
+  const SameDepatmentFacult = isacademicDepartmentExsist.academicFaculty;
+  if (SameDepatmentFacult !== academicFaculty) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `This ${isacademicDepartmentExsist.name} is  no exist this ${isacademicFacultyExsist.name}`
+    );
+  }
   /**
    * Step 1: check if the semester registration id is exists!
    * Step 2: check if the academic faculty id is exists!
@@ -28,6 +81,9 @@ const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
    * Step 10: create the offered course
    */
   //check if the semester registration id is exists!
+
+  const result = await OfferedCourse.create({ ...payload, academicSemester });
+  return result;
 };
 
 const getAllOfferedCoursesFromDB = async (query: Record<string, unknown>) => {};
